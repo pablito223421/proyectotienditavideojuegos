c873b1780dcf7906d07672bd94c05ee589affd0b
Pablo Hernandez Hernandez <pablohernandezhernandezuacm@gmail.com>
2021-12-09 16:54:57 -0600

    Ultimos 6 capitulos de Sistema de Categorias

--- my-app/api/Game.tsx
+++ my-app/api/Game.tsx
@@ -13,3 +13,30 @@ console.log(error);
 return null;
 }
 }
+
+export async function getGamesPlatformApi(platform,limit,start){
+    try {
+       const limitItems= `_limit=${limit}`; 
+       const sortItems= `_sort=createdAt:desc`;
+       const startItems= `_start=${start}`;
+       const url = `${BASE_PATH}/games?platform.url=${platform}&${limitItems}&${sortItems}&${startItems}`;
+       const response=await fetch(url); 
+       const result = await response.json();
+       return result;
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
+}
+
+export async function getTotalGamesPlatformApi(platform){
+    try {
+        const url= `${BASE_PATH}/games/count?platform.url=${platform}`;
+        const response =await fetch(url);
+        const result = await response.json();
+        return result;
+    } catch (error) {
+       console.log(error);
+       return null;       
+    }
+}
